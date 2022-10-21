9ee82c390422f631e1142ea844036a8f861bb893
Kevin Gilliam <keving@arizona.edu>
2022-10-21 14:59:14 -0700

    First commit

---
+++ 01_PMC_Driver/lfast_pmc_driver.xml.cmake
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+   <devGroup group="Telescopes">
+      <device label="LFAST Primary Mirror Driver" manufacturer="Steward Observatory ETS">
+         <driver name="LFAST Primary Mirror Driver">lfast_mount_driver</driver>
+         <version>@CDRIVER_VERSION_MAJOR@.@CDRIVER_VERSION_MINOR@</version>
+      </device>
+   </devGroup>
+</driversList>
+
